227f341e3be854b0c807f15ece4070c685987524
Prem Kumar <118460551+premkr7370@users.noreply.github.com>
2024-08-16 00:01:35 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function sumOfTwoNumber(a,b){
+      const sum=a+b;
+      console.log("result is",sum);
+      return sum;
+}
+const a=12;
+const b=13;
+const result=sumOfTwoNumber(a,b);
+console.log("return result",result);
